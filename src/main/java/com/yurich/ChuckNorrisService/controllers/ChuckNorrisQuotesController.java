5f214d278469b4ad89c005805aeddbea70ad087e
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2023-03-17 00:00:16 +0100

    Add Chuck Norris controller and page

---
+++ src/main/java/com/yurich/ChuckNorrisService/controllers/ChuckNorrisQuotesController.java
@@ -0,0 +1,23 @@
+package com.yurich.ChuckNorrisService.controllers;
+
+import com.yurich.ChuckNorrisService.services.ChuckNorrisQuotesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChuckNorrisQuotesController {
+
+    private final ChuckNorrisQuotesService service;
+
+    public ChuckNorrisQuotesController(ChuckNorrisQuotesService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/")
+    public String getRandomQuote(Model model) {
+        String joke = service.getRandomQuote();
+        model.addAttribute("joke", joke);
+        return "index";
+    }
+}
